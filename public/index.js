ceeec7348bcb683dec4179377fb7a3911ad1878b
AcademicV1ctim <nathaniel0911@gmail.com>
2025-03-23 23:11:31 +0800

    added the authentication qr code

---
+++ public/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const authUrl = "https://accounts.spotify.com/authorize?client_id=d831b0f307a1448b8ba4aa26c71f1496&response_type=code&redirect_uri=https://spotify-player-esp32.onrender.com/callback&scope=user-read-playback-state%20user-read-currently-playing%20user-modify-playback-state";
+    
+    new QRCode(document.getElementById("qrcode"), {
+      text: authUrl,
+      width: 256,
+      height: 256,
+      colorDark : "#000000",
+      colorLight : "#ffffff",
+      correctLevel : QRCode.CorrectLevel.H
+    });
+  });
+  
